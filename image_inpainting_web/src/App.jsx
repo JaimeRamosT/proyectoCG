f3b726eae7c5602b8f4577be6e901d04afd0a269
Gabriel Espinoza <gabrielespinoza@Gabriels-MacBook-Air.local>
2024-12-05 08:24:22 -0500

    README en frontend

--- image_inpainting_web/src/App.jsx
+++ image_inpainting_web/src/App.jsx
@@ -1,42 +1,22 @@
 import { useState } from 'react'
-import Canvas from './components/canvas'
-import UploadImage from './components/UploadImage';
-import Results from './components/Results';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Tryitout from './components/Tryitout'
+import Navbar from './components/Navbar'
+import Readme from './components/Readme'
 
 function App() {
-  const [image, setImage] = useState(null); // Imagen cargada por el usuario
-  const [outputImage, setOutputImage] = useState(null); // Resultado del modelo
-
-  const handleImageUpload = (file) => {
-    const url = URL.createObjectURL(file);
-    setImage(url); // Guarda la URL de la imagen
-  };
-
   return (
-    <>
-      <div className='min-h-screen w-full px-2 flex flex-col justify-center items-center bg-[#111827]'>
-        <h1 className='text-4xl font-bold text-white'>Residual Dense Image Inpainting CNN</h1>
-        <div className='min-h-[90vh] flex flex-col justify-center items-center w-full max-w-[80%] '>
-          {!image ? (
-            <UploadImage onImageUpload={handleImageUpload} />
-          ) : (
-            <div className='flex gap-5 w-full h-full'>
-              <Canvas 
-                image={image} 
-                handleRemoveImage={() => { 
-                  setImage(null);
-                  setOutputImage(null);
-                }} 
-                handleOutputImage={(result) => {
-                  setOutputImage(result);  // Guardamos solo el base64
-                }}
-              />
-              <Results outputImageBase64={outputImage}></Results>
-            </div>
-          )}
+    <BrowserRouter>
+      <>
+        <div className='min-h-screen w-full flex flex-col justify-center items-center bg-[#0d1116]'>
+          <Navbar />
+          <Routes>
+            <Route path="/tryit" element={<Tryitout />} />
+            <Route path="/" element={<Readme />} />
+          </Routes>
         </div>
-      </div>
-    </>
+      </>
+    </BrowserRouter>
   )
 }
 
